cba735ff869fef1446372500fc60d969e24df5a9
cku2411 <81894550+Cku2411@users.noreply.github.com>
2023-10-13 10:40:57 +0700

    UI chat-ap

--- chat-app/client.js
+++ chat-app/client.js
@@ -1,24 +1,66 @@
 const net = require("net");
+const readline = require("readline/promises");
 
 const hostname = "127.0.0.1";
 const port = 4080;
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const clearLine = (dir) => {
+  return new Promise((res, rej) => {
+    process.stdout.clearLine(dir, () => {
+      res();
+    });
+  });
+};
+
+const moveCursor = (dx, dy) => {
+  return new Promise((res, rej) => {
+    process.stdout.moveCursor(dx, dy, () => {
+      res();
+    });
+  });
+};
 
 const client = net.createConnection(
   {
     host: hostname,
     port: port,
   },
-  () => {
+  async () => {
     console.log("Connect to the server");
+
+    ask();
   }
 );
-
-client.on("close", () => {
-  console.log("Closed");
-});
 
 client.on("end", () => {
   console.log("ENDED");
 });
 
+client.on("data", async (data) => {
+  // log empty line
+  console.log();
+  // move cursor up
+  await moveCursor(0, -1);
+  // clean the line
+  await clearLine(0);
+  // log the message
+  console.log(data.toString());
+  ask();
+});
+
 // 2:53
+const ask = async () => {
+  const message = await rl.question("Enter a message:");
+  // move cursor one line up
+  await moveCursor(0, -1);
+  // clear the chat line
+  await clearLine(0);
+  client.write(message, (err) => {
+    if (err) console.log(err);
+  });
+};
--- chat-app/server.js
+++ chat-app/server.js
@@ -1,13 +1,25 @@
 const net = require("net");
 
 const server = net.createServer();
+const clients = [];
 
 const port = 4080;
 const hostname = "127.0.0.1";
 
-server.on("connection", (socket) => {
-  //   socket.write();
-  console.log("A new connection to the server");
+server.on("connection", (client) => {
+  // eveyr connection we have client object
+  //   client is a duplex stream so it can write and read
+  console.log("A new client connection to the server");
+
+  client.on("data", (data) => {
+    // everytime we receive data, we write all data to client
+    const dataToString = data.toString();
+    clients.map((cl) => {
+      cl.write(`${cl}: ${dataToString}`);
+    });
+  });
+
+  clients.push(client);
 });
 
 // Lisiten
