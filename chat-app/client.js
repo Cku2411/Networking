b7f83e6b2e5f4af10e312a7a79b14f4cc5cd9c13
cku2411 <81894550+Cku2411@users.noreply.github.com>
2023-10-13 15:09:18 +0700

    done server

--- chat-app/client.js
+++ chat-app/client.js
@@ -2,12 +2,14 @@ const net = require("net");
 const readline = require("readline/promises");
 
 const hostname = "127.0.0.1";
-const port = 4080;
+const port = 5000;
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+let id;
 
 const clearLine = (dir) => {
   return new Promise((res, rej) => {
@@ -48,19 +50,29 @@ client.on("data", async (data) => {
   await moveCursor(0, -1);
   // clean the line
   await clearLine(0);
-  // log the message
-  console.log(data.toString());
+
+  if (data.toString().substring(0, 2) === "id") {
+    // When we are getting an ID from server
+    id = data.toString().substring(3);
+    console.log(`Your id is ${id}! \n`);
+  } else {
+    // WHen we are getting an message
+
+    // log the message
+    console.log(data.toString());
+  }
   ask();
 });
 
 // 2:53
 const ask = async () => {
-  const message = await rl.question("Enter a message:");
+  const message = await rl.question("Enter a message > ");
+  const messageSent = `${id}-message&${message}`;
   // move cursor one line up
   await moveCursor(0, -1);
   // clear the chat line
   await clearLine(0);
-  client.write(message, (err) => {
+  client.write(messageSent, (err) => {
     if (err) console.log(err);
   });
 };
--- chat-app/server.js
+++ chat-app/server.js
@@ -1,25 +1,45 @@
 const net = require("net");
 
 const server = net.createServer();
+
 const clients = [];
 
-const port = 4080;
+const port = 5000;
 const hostname = "127.0.0.1";
 
 server.on("connection", (client) => {
   // eveyr connection we have client object
   //   client is a duplex stream so it can write and read
   console.log("A new client connection to the server");
+  // Getting an ID and sent it back to client.
+  const clientId = clients.length + 1;
+  client.write(`id-${clientId}`);
+  // notify all user about new user
+  clients.map((cl) => {
+    cl.client.write(`User ${clientId} joined!`);
+  });
+  clients.push({ id: clientId.toString(), client });
 
+  // -- listen to event
   client.on("data", (data) => {
     // everytime we receive data, we write all data to client
     const dataToString = data.toString();
-    clients.map((cl) => {
-      cl.write(`${cl}: ${dataToString}`);
+    const id = dataToString.substring(0, dataToString.indexOf("-"));
+    const message = dataToString.substring(dataToString.indexOf("&") + 1);
+    clients.map((cl, i) => {
+      cl.client.write(`User ${id}: ${message}`);
     });
   });
 
-  clients.push(client);
+  client.on("error", (err) => {
+    if (err.code === "ECONNRESET") {
+      clients.map((cl) => {
+        cl.client.write(`User ${clientId} left!`);
+      });
+    } else {
+      console.log(err);
+    }
+  });
 });
 
 // Lisiten
